735a9f680743ae00642322c74ea46dc9aa0af0d9
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-05-23 17:45:06 -0300

    Create Repository

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+//JPA REPOSITORY - EFETUA AS TRANSAÇÕES COM O BANCO DE DADOS. FACILITA POIS NÃO PRECISAMOS
+//CRIAR AS QUERYS NA MÃO
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+    
+}

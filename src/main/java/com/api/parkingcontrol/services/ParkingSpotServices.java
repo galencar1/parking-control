f2729dda2b55363ccc5ff31c0eae7dad369af5cf
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-05-24 08:51:27 -0300

    Create Class Services

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotServices.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotServices.java
@@ -1,5 +1,8 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //Service será a camada intermediária entre o controller e o Model.
@@ -9,5 +12,10 @@ import org.springframework.stereotype.Service;
 
 public class ParkingSpotServices {
     //Ocorrerá a comunicação entre o service e o repository através da injeção de dependencia.
-        
+    //@Autowired // Bean do Spring que efetua a Injeção de dependencia automaticamente.
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotServices(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
 }

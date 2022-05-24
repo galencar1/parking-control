e0a71e28f0a8ba1a7fa3c670e2080245bb87b8cd
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-05-24 08:18:11 -0300

    Create Class Services

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotServices.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.stereotype.Service;
+
+//Service será a camada intermediária entre o controller e o Model.
+//Pode ser feito uma interface do service e uma classe implementando a interface.
+
+@Service
+
+public class ParkingSpotServices {
+    //Ocorrerá a comunicação entre o service e o repository através da injeção de dependencia.
+        
+}
